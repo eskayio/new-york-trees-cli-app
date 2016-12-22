05ea6900815b942d5ce1ccce37607ae0945c6fe0
Samir Singh <singhstart@gmail.com>
2016-12-22 15:00:42 +0000

    add project file structure, environment.rb, cli driver file

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative '../lib/new-york-trees/scraper'
+require_relative '../lib/new-york-trees/tree'
+require_relative '../lib/new-york-trees/cli'
---
+++ lib/new-york-trees.rb
@@ -0,0 +1,4 @@
+module NewYorkTrees
+end
+
+require_relative '../config/environment'
