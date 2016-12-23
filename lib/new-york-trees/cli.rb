ea3e9517b1226d5e2aa782362afd92367bf56f0d
Samir Singh <singhstart@gmail.com>
2016-12-23 23:05:45 +0000

    add tree details to #print_tree method in cli.rb

--- lib/new-york-trees/cli.rb
+++ lib/new-york-trees/cli.rb
@@ -17,20 +17,31 @@ class NewYorkTrees::CLI
   def print_trees(number_from_user)
     puts "---------------------Trees #{input} - #{input + 10}---------------------"
     puts ""
-    array_pos = input - 1
-    puts "#{array_pos}.  #{NewYorkTrees::Tree.all[array_pos].name} (#{NewYorkTrees::Tree.all[array_pos].scientific_name})"
-    puts "#{array_pos + 1}.  #{NewYorkTrees::Tree.all[array_pos].name} (#{NewYorkTrees::Tree.all[array_pos].scientific_name})"
-    puts "#{array_pos + 2}.  #{NewYorkTrees::Tree.all[array_pos].name} (#{NewYorkTrees::Tree.all[array_pos].scientific_name})"
-    puts "#{array_pos + 3}.  #{NewYorkTrees::Tree.all[array_pos].name} (#{NewYorkTrees::Tree.all[array_pos].scientific_name})"
-    puts "#{array_pos + 4}.  #{NewYorkTrees::Tree.all[array_pos].name} (#{NewYorkTrees::Tree.all[array_pos].scientific_name})"
-    puts "#{array_pos + 5}.  #{NewYorkTrees::Tree.all[array_pos].name} (#{NewYorkTrees::Tree.all[array_pos].scientific_name})"
-    puts "#{array_pos + 6}.  #{NewYorkTrees::Tree.all[array_pos].name} (#{NewYorkTrees::Tree.all[array_pos].scientific_name})"
-    puts "#{array_pos + 7}.  #{NewYorkTrees::Tree.all[array_pos].name} (#{NewYorkTrees::Tree.all[array_pos].scientific_name})"
-    puts "#{array_pos + 8}.  #{NewYorkTrees::Tree.all[array_pos].name} (#{NewYorkTrees::Tree.all[array_pos].scientific_name})"
-    puts "#{array_pos + 9}.  #{NewYorkTrees::Tree.all[array_pos].name} (#{NewYorkTrees::Tree.all[array_pos].scientific_name})"     
+    @array_pos = input - 1
+    puts "#{@array_pos}.  #{NewYorkTrees::Tree.all[@array_pos].name} (#{NewYorkTrees::Tree.all[array_pos].scientific_name})"
+    puts "#{@array_pos + 1}.  #{NewYorkTrees::Tree.all[@array_pos].name} (#{NewYorkTrees::Tree.all[array_pos].scientific_name})"
+    puts "#{@array_pos + 2}.  #{NewYorkTrees::Tree.all[@array_pos].name} (#{NewYorkTrees::Tree.all[array_pos].scientific_name})"
+    puts "#{@array_pos + 3}.  #{NewYorkTrees::Tree.all[@array_pos].name} (#{NewYorkTrees::Tree.all[array_pos].scientific_name})"
+    puts "#{@array_pos + 4}.  #{NewYorkTrees::Tree.all[@array_pos].name} (#{NewYorkTrees::Tree.all[array_pos].scientific_name})"
+    puts "#{@array_pos + 5}.  #{NewYorkTrees::Tree.all[@array_pos].name} (#{NewYorkTrees::Tree.all[array_pos].scientific_name})"
+    puts "#{@array_pos + 6}.  #{NewYorkTrees::Tree.all[@array_pos].name} (#{NewYorkTrees::Tree.all[array_pos].scientific_name})"
+    puts "#{@array_pos + 7}.  #{NewYorkTrees::Tree.all[@array_pos].name} (#{NewYorkTrees::Tree.all[array_pos].scientific_name})"
+    puts "#{@array_pos + 8}.  #{NewYorkTrees::Tree.all[@array_pos].name} (#{NewYorkTrees::Tree.all[array_pos].scientific_name})"
+    puts "#{@array_pos + 9}.  #{NewYorkTrees::Tree.all[@array_pos].name} (#{NewYorkTrees::Tree.all[array_pos].scientific_name})"
   end
 
   def print_tree(id)
+    puts "---------------------Trees #{NewYorkTrees::Tree.all[@array_pos].name} - #{input + 9}---------------------"
+    puts ""
+    puts "Common Name:                #{NewYorkTrees::Tree.all[@array_pos].name}"
+    puts "Scientific Name:            #{NewYorkTrees::Tree.all[@array_pos].scientific_name}"
+    puts "Description:                #{NewYorkTrees::Tree.all[@array_pos].description}"
+    puts "Bark:                       #{NewYorkTrees::Tree.all[@array_pos].bark}"
+    puts "Twigs:                      #{NewYorkTrees::Tree.all[@array_pos].twigs}"
+    puts "Winter Buds:                #{NewYorkTrees::Tree.all[@array_pos].winter_buds}"
+    puts "Leaves:                     #{NewYorkTrees::Tree.all[@array_pos].leaves}"
+    puts "Fruit:                      #{NewYorkTrees::Tree.all[@array_pos].fruit}"
+    puts "Distinguishing Features:    #{NewYorkTrees::Tree.all[@array_pos].distinguishing_features}"
   end
 
 end
