0f8e8ce5730b3cebc9a3686eea40481775d6b095
Samir Singh <singhstart@gmail.com>
2016-12-24 06:28:06 +0000

    modify #trees_from_index to more accurate #scrape_trees_index

--- lib/new-york-trees/scraper.rb
+++ lib/new-york-trees/scraper.rb
@@ -1,22 +1,16 @@
-#replace NewYorkTrees::
-require 'pry'
-require 'nokogiri'
-require 'open-uri'
 
-class Scraper
+class NewYorkTrees::Scraper
   def get_page
     Nokogiri::HTML(open("http://bhort.bh.cornell.edu/tree/list.htm"))
   end
 
-  def trees_from_index
-    #returns trees list as Nokogiri nodeset of tree html nodes
+  def scrape_trees_index
     self.get_page.css('li')
   end
 
   def make_trees
     #loops through return value from #trees_from_index and constructs a tree from each tree node using #make_from_index
+
   end
 
 end
-
-Scraper.new.trees_from_index
