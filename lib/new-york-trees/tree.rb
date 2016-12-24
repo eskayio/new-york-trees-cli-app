d5ec42752a238b9cc4c0a0a878bc6a2d9da90a06
Samir Singh <singhstart@gmail.com>
2016-12-24 06:31:59 +0000

    remove @description from #initialize method in tree.rb

--- lib/new-york-trees/tree.rb
+++ lib/new-york-trees/tree.rb
@@ -9,13 +9,12 @@ class NewYorkTrees::Tree
     # add class constructor here -- taking input from scraper.rb #make_trees method
   end
 
-  def initialize(name=nil,scientific_name=nil,description=nil)
+  def initialize(name=nil,scientific_name=nil)
     @name = name
     @scientific_name = scientific_name
-    @description = description
     @@all << self
   end
 
-  
+
 
 end
